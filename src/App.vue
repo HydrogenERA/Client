dcd8e9ac0008bd21e2b99ea1f7cca0f6ccf9edde
Richard Khonan <richardkhonan0797@gmail.com>
2019-12-05 18:27:06 +0700

    add tweet button

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <sharePic :imageId="image"></sharePic>
+</div>
+</template>
+
+<script>
+import sharePic from './components/sharePic'
+export default {
+  name:'App',
+  components:{
+      sharePic
+  },
+  data() {
+    return {
+      image:''
+    };
+  },
+};
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/sharePic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a
+      :href="shareLink"
+      class="twitter-share-button"
+      data-size="large"
+      data-show-count="false"
+    >Tweet</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sharePic",
+  data() {
+    return {
+        shareLink:`https://twitter.com/intent/tweet?text=https://imgur.com/gallery/u2QPHwe`
+    };
+  },
+  props:['imageId']
+};
+</script>
+
+<style>
+</style>
